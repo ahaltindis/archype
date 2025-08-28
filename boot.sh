8c63c8823b5a90bfdd9148192c6191c83be0c2fd
Ahmet <ahaltindis@gmail.com>
2025-08-28 23:49:02 +0100

    Start updating via logo! and few repo details

--- boot.sh
+++ boot.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
-ansi_art='                 ▄▄▄                                                   
- ▄█████▄    ▄███████████▄    ▄███████   ▄███████   ▄███████   ▄█   █▄    ▄█   █▄ 
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   █▀   ███   ███  ███   ███
-███   ███  ███   ███   ███ ▄███▄▄▄███ ▄███▄▄▄██▀  ███       ▄███▄▄▄███▄ ███▄▄▄███
-███   ███  ███   ███   ███ ▀███▀▀▀███ ▀███▀▀▀▀    ███      ▀▀███▀▀▀███  ▀▀▀▀▀▀███
-███   ███  ███   ███   ███  ███   ███ ██████████  ███   █▄   ███   ███  ▄██   ███
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
- ▀█████▀    ▀█   ███   █▀   ███   █▀   ███   ███  ███████▀   ███   █▀    ▀█████▀ 
-                                       ███   █▀                                  '
+ansi_art='
+ █████╗ ██████╗  ██████╗██╗  ██╗██╗   ██╗██████╗ ███████╗
+██╔══██╗██╔══██╗██╔════╝██║  ██║╚██╗ ██╔╝██╔══██╗██╔════╝
+███████║██████╔╝██║     ███████║ ╚████╔╝ ██████╔╝█████╗  
+██╔══██║██╔══██╗██║     ██╔══██║  ╚██╔╝  ██╔═══╝ ██╔══╝  
+██║  ██║██║  ██║╚██████╗██║  ██║   ██║   ██║     ███████╗
+╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚═╝     ╚══════╝'
 
 clear
 echo -e "\n$ansi_art\n"
 
 sudo pacman -Syu --noconfirm --needed git
 
-# Use custom repo if specified, otherwise default to basecamp/omarchy
-OMARCHY_REPO="${OMARCHY_REPO:-basecamp/omarchy}"
+# Use custom repo if specified, otherwise default to ahaltindis/archype
+OMARCHY_REPO="${OMARCHY_REPO:-ahaltindis/archype}"
 
-echo -e "\nCloning Omarchy from: https://github.com/${OMARCHY_REPO}.git"
+echo -e "\nCloning Archype from: https://github.com/${OMARCHY_REPO}.git"
 rm -rf ~/.local/share/omarchy/
 git clone "https://github.com/${OMARCHY_REPO}.git" ~/.local/share/omarchy >/dev/null
 
-# Use custom branch if instructed, otherwise default to master
-OMARCHY_REF="${OMARCHY_REF:-master}"
-if [[ $OMARCHY_REF != "master" ]]; then
+# Use custom branch if instructed, otherwise default to main
+OMARCHY_REF="${OMARCHY_REF:-main}"
+if [[ $OMARCHY_REF != "main" ]]; then
   echo -e "\eUsing branch: $OMARCHY_REF"
   cd ~/.local/share/omarchy
   git fetch origin "${OMARCHY_REF}" && git checkout "${OMARCHY_REF}"

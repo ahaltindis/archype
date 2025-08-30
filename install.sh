970bdda5c1ab09ac4170e322ea41e315e031b8b4
Ahmet Altindis <ahaltindis@gmail.com>
2025-08-30 17:48:30 +0100

    I don't want whole config copy

--- install.sh
+++ install.sh
@@ -24,7 +24,6 @@ source $OMARCHY_INSTALL/config/hardware/nvidia.sh
 
 source $OMARCHY_INSTALL/config/branding.sh
 source $OMARCHY_INSTALL/config/terminal.sh
-# source $OMARCHY_INSTALL/config/config.sh
 # source $OMARCHY_INSTALL/config/theme.sh
 # source $OMARCHY_INSTALL/config/git.sh
 # source $OMARCHY_INSTALL/config/gpg.sh
